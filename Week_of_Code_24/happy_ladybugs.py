1f4625ea3261d85af3f880db8281d5dc606f278e
Pedro Rodrigues <csixteen@gmail.com>
2016-10-22 14:50:21 +0200

    Initial commit

---
+++ Week_of_Code_24/happy_ladybugs.py
@@ -0,0 +1,21 @@
+def happy_bugs(b, n):
+    # Only underscores: they are happy
+    if b.count("_") == n:
+        return "YES"
+
+    if b.count("_") > 0:
+        b = sorted(b)
+    for i in range(n):
+        if (i == 0) and (b[i] != "_") and(b[i] != b[i+1]):
+            return "NO"
+        if (i == n-1) and (b[i] != "_") and (b[i] != b[i-1]):
+            return "NO"
+        if (0 < i < n-1) and (b[i] != b[i-1]) and (b[i] != b[i+1]):
+            return "NO"
+    return "YES"
+
+
+g = int(input())
+for i in range(g):
+    n, b = int(input()), input()
+    print(happy_bugs(b, n))
